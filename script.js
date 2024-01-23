1351496974ab0457022af93e8d5b63dfabe6de2b
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-01-23 16:56:41 +0100

    create deletetask function and fixed internal datas

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ console.log('vue ok', Vue);
 
 const {createApp} = Vue;
 const app = createApp({
-    name: 'TodoList',
     data: () => ({
-        tasks,
-    })
-})
+        tasks: [
+            { id: 1, text: 'Fare la spesa'},
+            { id: 2, text: 'Lavare i capelli' },
+            { id: 3, text: 'Comprare una marca da bollo' },
+            { id: 4, text: 'Aggiornare il PC' }
+        ]
+    }),
+    methods: {
+        deleteTask(id) {
+            this.tasks = this.tasks.filter(task => id !== task.id);
+        }
+    }
+});
 app.mount('#root');
