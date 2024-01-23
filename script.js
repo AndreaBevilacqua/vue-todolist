d6f1e530eb1f1d0e45e356a4efc39223da8a6b77
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-01-23 17:16:17 +0100

    added addtask function

--- script.js
+++ script.js
@@ -8,11 +8,20 @@ const app = createApp({
             { id: 2, text: 'Lavare i capelli' },
             { id: 3, text: 'Comprare una marca da bollo' },
             { id: 4, text: 'Aggiornare il PC' }
-        ]
+        ],
+
+        newTaskInput: ''
+
     }),
     methods: {
         deleteTask(id) {
             this.tasks = this.tasks.filter(task => id !== task.id);
+        },
+
+        addTask() {
+            const newTask = { id: 5, done: false, text: this.newTaskInput}
+            this.tasks.push(newTask);
+            this.newTaskInput = '';
         }
     }
 });
